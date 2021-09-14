5ae71273023d68780ea39e61d8d2865958a8a411
Nekozouneko Team Lab <89770962+NekozounekoTEAM@users.noreply.github.com>
2021-09-14 20:20:16 +0900

    p1.0.1/1.17.1-R0.1-SNAPSHOTのファイルをアップロード

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# NUtils
-Spigot / Bukkit APIで動くユーティリティプラグインです。(Not Supported English :（ )
+### **不安定版のため、思いにもよらないバグが起きる可能性がありますのでご注意ください。**
+開発: @Taitaitatata
+
+## コマンド
+
+#### `/nfly`
+浮遊をサバイバルやアドベンチャーで適応します。
+
+![image](https://user-images.githubusercontent.com/89770962/132973937-59c89f96-98f5-46d0-b30a-67d11049a4cb.png)
+
+![image](https://user-images.githubusercontent.com/89770962/132973953-7de95425-2369-4ae4-abe5-e342d7c35418.png)
+
+
+権限: `NUtils.commands.nickname` or OP Lv.4
+
+#### `/nickname <Name : String>`
+サーバー内でニックネームを設定します。
+
+![image](https://user-images.githubusercontent.com/89770962/132974016-cb4f4c68-9a13-4f76-bed6-59d76fb8a4fe.png)
+
+(Tabのプレイヤーリストやコマンドログでは適応されません)
+
+
+権限: `NUtils.commands.fly` or OP Lv.4
+
+#### `/gamemode <Gamemode> [<target>]`
+(New Command)
+
+1.14以前のgamemodeに復元するコマンドです。今のオプションも利用可能です。
+
+<picture>
+
+(1.14以降のgamemodeを実行するには `/minecraft:gamemode` で実行できます)
+
+権限: `NUtils.commands.gamemode` or OP Lv.4
+
+
+## 改造について
+MITライセンスの上、改造が可能です。
+
+NUtils (C) 2021 Nekozouneko Team. All Rights Reserved.
--- src/main/java/ga/nekozouneko/plugins/utils/nutils/NUtils.java
+++ src/main/java/ga/nekozouneko/plugins/utils/nutils/NUtils.java
@@ -1,6 +1,7 @@
 package ga.nekozouneko.plugins.utils.nutils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -16,18 +17,16 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
-public class NUtils extends JavaPlugin implements Listener {
-
-
+public final class NUtils extends JavaPlugin implements Listener {
     // 起動 / 終了時の表示テキスト
 
     @Override
     public void onEnable() {
-        saveDefaultConfig();
-        FileConfiguration config = getConfig();
         getLogger().info("§n§6[§bNUtils§6]§r §l§4>§6>§r NUtils for Spigot Edition Running on " + getServer().getName());
         getLogger().info("§n§6[§bNUtils§6]§r §l§4>§6>§r §aEnabled !");
     }
@@ -42,6 +41,15 @@ public class NUtils extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        for (Player allOP : getServer().getOnlinePlayers()) {
+            if (allOP.isOp()) {
+                if (args.length == 1) {
+                    allOP.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r " + sender.getName() + " が §7§n/" + command.getName() + " " + args[0] + "§r を使用しました");
+                } else {
+                    allOP.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r " + sender.getName() + " が §7§n" + command.getName() + " " + args[0] + " " + args[1] +"§rを使用しました");
+                }
+            }
+        }
         if (command.getName().equalsIgnoreCase("nickname")) {
             String cn = args[0].replace("&", "§");
             getServer().getPlayer(sender.getName()).setDisplayName(cn);
@@ -61,8 +69,64 @@ public class NUtils extends JavaPlugin implements Listener {
                 getServer().getPlayer(sender.getName()).setAllowFlight(true);
                 sender.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r 浮遊を有効化しました。");
             }
+        } else if (command.getName().equalsIgnoreCase("gamemode")) {
+            if (args.length == 2) {
+                if (args[0].equalsIgnoreCase("adventure") || args[0].equalsIgnoreCase("a") || args[0].equalsIgnoreCase("2")) {
+                    getServer().getPlayer(args[1]).setGameMode(GameMode.ADVENTURE);
+                    sender.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r ゲームモードを§n§7アドベンチャーモード§rに変更しました");
+                    return true;
+                } else if (args[0].equalsIgnoreCase("survival") || args[0].equalsIgnoreCase("s") || args[0].equalsIgnoreCase("0")) {
+                    getServer().getPlayer(args[1]).setGameMode(GameMode.SURVIVAL);
+                    sender.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r ゲームモードを§n§7クリエイティブモード§rに変更しました");
+                    return true;
+                } else if (args[0].equalsIgnoreCase("creative") || args[0].equalsIgnoreCase("c") || args[0].equalsIgnoreCase("1")) {
+                    getServer().getPlayer(args[1]).setGameMode(GameMode.CREATIVE);
+                    sender.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r ゲームモードを§n§7クリエイティブモード§rに変更しました");
+                    return true;
+                } else if (args[0].equalsIgnoreCase("spectator") || args[0].equalsIgnoreCase("sp") || args[0].equalsIgnoreCase("3")) {
+                    getServer().getPlayer(args[1]).setGameMode(GameMode.SPECTATOR);
+                    sender.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r ゲームモードを§n§7スペクテイターモード§rに変更しました");
+                    return true;
+                }
+            }
+            if (args[0].equalsIgnoreCase("adventure") || args[0].equalsIgnoreCase("a") || args[0].equalsIgnoreCase("2")) {
+                getServer().getPlayer(sender.getName()).setGameMode(GameMode.ADVENTURE);
+                sender.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r ゲームモードを§n§7アドベンチャーモード§rに変更しました");
+                return true;
+            } else if (args[0].equalsIgnoreCase("survival") || args[0].equalsIgnoreCase("s") || args[0].equalsIgnoreCase("0")) {
+                getServer().getPlayer(sender.getName()).setGameMode(GameMode.SURVIVAL);
+                sender.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r ゲームモードを§n§7クリエイティブモード§rに変更しました");
+                return true;
+            } else if (args[0].equalsIgnoreCase("creative") || args[0].equalsIgnoreCase("c") || args[0].equalsIgnoreCase("1")) {
+                getServer().getPlayer(sender.getName()).setGameMode(GameMode.CREATIVE);
+                sender.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r ゲームモードを§n§7クリエイティブモード§rに変更しました");
+                return true;
+            } else if (args[0].equalsIgnoreCase("spectator") || args[0].equalsIgnoreCase("sp") || args[0].equalsIgnoreCase("3")) {
+                getServer().getPlayer(sender.getName()).setGameMode(GameMode.SPECTATOR);
+                sender.sendMessage("§n§6[§bNUtils§6]§r §l§4>§6>§r ゲームモードを§n§7スペクテイターモード§rに変更しました");
+                return true;
+            }
         }
-        return true;
+         return true;
+    }
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (!command.getName().equalsIgnoreCase("gamemode")) return super.onTabComplete(sender, command, alias, args);
+        if (args.length == 1) {
+            if (args[0].length() == 0) { // /testまで
+                return Arrays.asList("adventure", "creative", "survival", "spectator", "a", "c", "s", "sp", "0", "1", "2", "3");
+            } else if (args[0].startsWith("a")) {
+                    return Arrays.asList("adventure", "a");
+                } else if (args[0].startsWith("c")) {
+                    return Arrays.asList("creative", "c");
+                } else if (args[0].startsWith("s")) {
+                    return Arrays.asList("survival", "s");
+                } else if (args[0].startsWith("sp")) {
+                    return Arrays.asList("spectator", "sp");
+            }
+        }
+        //JavaPlugin#onTabComplete()を呼び出す
+        return super.onTabComplete(sender, command, alias, args);
     }
 }
 
